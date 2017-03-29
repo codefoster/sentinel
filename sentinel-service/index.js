6b2788dfb7b68c8ff767eec5fdd2edb349d6f622
Pierre Cauchois <pierre.cauchois@gmail.com>
2017-03-29 14:39:48 -0700

    Event Hubs code to listen to the sentinel route

--- sentinel-service/index.js
+++ sentinel-service/index.js
@@ -2,9 +2,8 @@ var iothub = require('azure-iothub');
 var common = require('azure-iot-common');
 let eventhubs = require('azure-event-hubs');
 
-var connectionString = process.env.IOTHUB_CONNECTION_STRING;
+var connectionString = process.env.EVENTHUB_CONNECTION_STRING;
 var eventhubsClient = eventhubs.Client.fromConnectionString(connectionString);
-var iothubClient = iothub.Client.fromConnectionString(connectionString);
 
 //open event hubs client for handling D2C messages
 eventhubsClient.open()
@@ -15,20 +14,16 @@ eventhubsClient.open()
         pids.map(pid =>
             eventhubsClient.createReceiver('$Default', pid, { 'startAfterTime': Date.now() })
                 .then(receiver =>
-                    //handle D2C messages
-                    receiver.on('message', msg =>
-                        console.log('D2C message received <--')
-                    )
+                    //handle Sentinel messages
+                    receiver.on('message', msg => {
+                        console.log('--> Sentinel message received <--');
+                        console.log('-- Properties --');
+                        console.log(JSON.stringify(msg.applicationProperties, null, 2));
+                        console.log('-- Payload --');
+                        console.log(JSON.stringify(msg.body, null, 2));
+                        console.log('---------------------------------');
+                        
+                    })
                 )
         )
     );
-
-// //open iothub client for handling C2D messages
-// iothubClient.open(err => {
-//     // send a C2D message repeatedly
-//     setInterval(() => {
-//       var message = new common.Message(JSON.stringify({ text: 'reset-request' }));
-//       console.log('Sending C2D message -->');
-//       iothubClient.send('device1', message, (err, res) => { });
-//     }, 5000);
-// });
