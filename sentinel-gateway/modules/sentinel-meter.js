a1258d3c404ceee719ea3cc64e702c08681cd068
Jeremy Foster <jeremy.foster@live.com>
2017-03-29 16:22:46 -0700

    create sentinel messages for select values

--- sentinel-gateway/modules/sentinel-meter.js
+++ sentinel-gateway/modules/sentinel-meter.js
@@ -2,7 +2,6 @@
 
 let Rx = require('rxjs/Rx')
 let utf8 = require('./util').utf8
-let tokenbucket = require('./sentinel-tokenbucket');
 
 class SentinelMeterModule {
   create(broker, configuration) {
@@ -10,22 +9,26 @@ class SentinelMeterModule {
     this.configuration = configuration
     this.messages = new Rx.Subject()
     this.subscription = this.messages
-        //add any interesting Rx operators here (i.e. .map, .filter)
-      .subscribe(msg => {
-        //build and send a sentinel message
-        let sentinelMessage = {
-          properties: {
-            source:'sentinel-meter',
-            type:'sentinel-message'
-          },
-          content: utf8.encode('TBD')
-        };
-        this.broker.publish(sentinelMessage);
+      .groupBy(msg => msg.deviceId)
+      .subscribe(devices => {
+        devices.subscribe(msg => {
+          let content = JSON.parse(utf8.decode(msg.content));
+          if (content.property1 < 0.2) {
+            //build and send a sentinel message
+            let sentinelMessage = {
+              properties: {
+                source: 'sentinel-meter',
+                type: 'sentinel-message'
+              },
+              content: utf8.encode('TBD')
+            };
+            this.broker.publish(sentinelMessage);
+          }
 
-        //pass on the original message (may want to make this conditional)
-        this.broker.publish(msg);
-
-      });
+          //pass on the original message (may want to make this conditional)
+          this.broker.publish(msg);
+        })
+      })
     return true;
   }
 
