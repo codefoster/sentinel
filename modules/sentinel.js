02ce0d8ef0ff339f0243f679a7a198e5318c1e14
Jeremy Foster <jeremy.foster@live.com>
2017-03-29 11:04:06 -0700

    add stub for sentinel-tokenbucket

--- modules/sentinel.js
+++ modules/sentinel.js
@@ -2,6 +2,7 @@
 
 let Rx = require('rxjs/Rx')
 let utf8 = require('./util').utf8
+let tokenbucket = require('./sentinel-tokenbucket');
 
 class SentinelModule {
   create(broker, configuration) {
@@ -9,8 +10,8 @@ class SentinelModule {
     this.configuration = configuration
     this.messages = new Rx.Subject()
     this.subscription = this.messages
-      //do interesting things here
       .subscribe(msg => {
+        //do something interesting here with the token bucket module
         this.broker.publish(msg)
       });
     return true;
