a7961c064542c9e8eb8493a7e9f12265acecde62
Jeremy Foster <jeremy.foster@live.com>
2017-03-30 11:45:59 -0700

    add device key for gateway

--- sentinel-gateway/modules/sentinel-analyzer.js
+++ sentinel-gateway/modules/sentinel-analyzer.js
@@ -1,29 +1,30 @@
 'use strict'
 
-let Rx = require('rxjs/Rx')
-let utf8 = require('./util').utf8
+let Rx = require('rxjs/Rx');
+let utf8 = require('./util').utf8;
 
 class SentinelAnalyzerModule {
   create(broker, configuration) {
-    this.broker = broker
-    this.configuration = configuration
-    this.messages = new Rx.Subject()
+    this.broker = broker;
+    this.configuration = configuration;
+    this.messages = new Rx.Subject();
     this.subscription = this.messages
       .filter(msg => msg.properties.type == 'sentinel-message')
       .subscribe(msg => {
         //do something interesting here with sentinel messages
+        msg.properties.macAddress = "00:00:00:00:00:00"
         this.broker.publish(msg)
       });
     return true;
   }
 
   receive(msg) {
-    this.messages.next(msg)
+    this.messages.next(msg);
   }
 
   destroy() {
-    this.subscription.unsubscribe()
+    this.subscription.unsubscribe();
   }
 }
 
-module.exports = new SentinelAnalyzerModule()
+module.exports = new SentinelAnalyzerModule();
